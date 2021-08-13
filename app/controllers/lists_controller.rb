ec90973a40414e08cf64e7e6cd7444dc4801e946
Marie Hasselberg <m.hasselberg@posteo.de>
2021-08-13 15:12:11 +0200

    Controllers

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -12,8 +12,12 @@ class ListsController < ApplicationController
   end
 
   def create
-    @list = List.create(list_params)
-    # redirect_to @list, notice: 'New list was created.'
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
   end
 
   private
